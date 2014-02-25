a2b9b758ffcbc44419f83b139c4fbd06a9d60e6e
Freika <iamfrey@gmail.com>
2014-02-25 23:35:54 +0700

    deploy commit

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,6 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
-  #:registerable,
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :registerable,
 # link_to 'Edit profile', edit_admin_registration_path %> |
 end
