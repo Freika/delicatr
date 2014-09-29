024e34aebed4e9ac7adb5a3e88a19011d59be181
Freika <iamfrey@gmail.com>
2014-09-29 18:47:37 +0700

    Added: check post for existance

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -7,27 +7,12 @@ class Blog < ActiveRecord::Base
 
   def self.parse_blog(blog)
     parsed = Feedjira::Feed.fetch_and_parse(blog.feed_url)
-
     parsed.entries.each do |entry|
-      Post.create(title: entry.title, author: entry.author, body: entry.content,
-        link: entry.url, blog_id: blog.id, entry_id: entry.entry_id)
+      unless Post.exists?(entry_id: entry.id)
+        Post.create(title: entry.title, author: entry.author, body: entry.content,
+          link: entry.url, blog_id: blog.id, entry_id: entry.entry_id)
+      end
     end
   end
 
-  def update_feeds
-    urls = Blog.all.pluck(:feed_url)
-    feeds = Feedjira::Feed.fetch_and_parse urls
-    updated_feeds = Feedjira::Feed.update(feeds.values)
-    updated_feeds.each do |feed|
-
-    end
-
-  end
-  # updated_feed = updated_feeds['http://feedjira.com/blog/feed.xml']
-  # updated_feed.updated?
-  # updated_feed.new_entries
-
-  # def method_name
-
-  # end
 end
